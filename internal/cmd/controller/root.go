75b4740cd5a7db9870db7f234cfe97fda0043cea
bjunker <brian.junker@gmail.com>
2025-03-06 08:30:58 -0800

    Fix cmd import

--- internal/cmd/controller/root.go
+++ internal/cmd/controller/root.go
@@ -1,12 +1,10 @@
 package controller
 
 import (
-	"github.com/coding-ia/renovate-controller/cmd"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"log"
 	"os"
-
-	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
@@ -21,27 +19,27 @@ to quickly create a Cobra application.`,
 }
 
 func Execute() {
-	cmd.taskCmd.PersistentFlags().StringP("appId", "a", "", "GitHub Installation Application ID")
-	cmd.taskCmd.PersistentFlags().StringP("pem-aws-secret", "s", "", "GitHub Application Private Key (Secrets Manager)")
-	cmd.taskCmd.PersistentFlags().StringP("endpoint", "e", "", "GitHub Endpoint")
+	taskCmd.PersistentFlags().StringP("appId", "a", "", "GitHub Installation Application ID")
+	taskCmd.PersistentFlags().StringP("pem-aws-secret", "s", "", "GitHub Application Private Key (Secrets Manager)")
+	taskCmd.PersistentFlags().StringP("endpoint", "e", "", "GitHub Endpoint")
 
-	mapEnvToPFlag(cmd.taskCmd, "appId", "GITHUB_APPLICATION_ID")
-	mapEnvToPFlag(cmd.taskCmd, "pem-aws-secret", "GITHUB_APPLICATION_PRIVATE_PEM_AWS_SECRET")
-	mapEnvToPFlag(cmd.taskCmd, "endpoint", "GITHUB_APPLICATION_ENDPOINT")
+	mapEnvToPFlag(taskCmd, "appId", "GITHUB_APPLICATION_ID")
+	mapEnvToPFlag(taskCmd, "pem-aws-secret", "GITHUB_APPLICATION_PRIVATE_PEM_AWS_SECRET")
+	mapEnvToPFlag(taskCmd, "endpoint", "GITHUB_APPLICATION_ENDPOINT")
 
-	cmd.runCmd.Flags().StringP("cluster", "c", "", "ECS Cluster Name")
-	cmd.runCmd.Flags().StringP("task", "t", "", "Task Definition Name")
-	cmd.runCmd.Flags().String("container-name", "renovate", "Task Container Name")
-	cmd.runCmd.Flags().String("subnet-ids", "", "AWS VPC Subnet IDs")
-	cmd.runCmd.Flags().String("security-group-ids", "", "AWS VPC SecurityGroup IDs")
-	cmd.runCmd.Flags().Bool("assign-public-ip", false, "Assign Public IP to Task")
+	runCmd.Flags().StringP("cluster", "c", "", "ECS Cluster Name")
+	runCmd.Flags().StringP("task", "t", "", "Task Definition Name")
+	runCmd.Flags().String("container-name", "renovate", "Task Container Name")
+	runCmd.Flags().String("subnet-ids", "", "AWS VPC Subnet IDs")
+	runCmd.Flags().String("security-group-ids", "", "AWS VPC SecurityGroup IDs")
+	runCmd.Flags().Bool("assign-public-ip", false, "Assign Public IP to Task")
 
-	mapEnvToFlag(cmd.runCmd, "cluster", "AWS_ECS_CLUSTER_NAME")
-	mapEnvToFlag(cmd.runCmd, "task", "AWS_ECS_CLUSTER_TASK")
-	mapEnvToFlag(cmd.runCmd, "container-name", "AWS_ECS_CLUSTER_TASK_CONTAINER_NAME")
-	mapEnvToFlag(cmd.runCmd, "subnet-ids", "AWS_ECS_TASK_SUBNET_IDS")
-	mapEnvToFlag(cmd.runCmd, "security-group-ids", "AWS_ECS_TASK_SECURITY_GROUP_IDS")
-	mapEnvToFlag(cmd.runCmd, "assign-public-ip", "AWS_ECS_TASK_PUBLIC_IP")
+	mapEnvToFlag(runCmd, "cluster", "AWS_ECS_CLUSTER_NAME")
+	mapEnvToFlag(runCmd, "task", "AWS_ECS_CLUSTER_TASK")
+	mapEnvToFlag(runCmd, "container-name", "AWS_ECS_CLUSTER_TASK_CONTAINER_NAME")
+	mapEnvToFlag(runCmd, "subnet-ids", "AWS_ECS_TASK_SUBNET_IDS")
+	mapEnvToFlag(runCmd, "security-group-ids", "AWS_ECS_TASK_SECURITY_GROUP_IDS")
+	mapEnvToFlag(runCmd, "assign-public-ip", "AWS_ECS_TASK_PUBLIC_IP")
 
 	generateConfigCmd.Flags().Int64P("installationId", "", 0, "GitHub Installation ID")
 	generateConfigCmd.Flags().StringP("target-repository", "", "", "GitHub target repository")
@@ -55,9 +53,9 @@ func Execute() {
 	mapEnvToFlag(generateConfigCmd, "s3-config-key", "CONFIG_TEMPLATE_KEY")
 	mapEnvToFlag(generateConfigCmd, "output", "GENERATE_CONFIG_OUTPUT")
 
-	cmd.taskCmd.AddCommand(cmd.runCmd)
-	cmd.taskCmd.AddCommand(generateConfigCmd)
-	rootCmd.AddCommand(cmd.taskCmd)
+	taskCmd.AddCommand(runCmd)
+	taskCmd.AddCommand(generateConfigCmd)
+	rootCmd.AddCommand(taskCmd)
 
 	err := rootCmd.Execute()
 	if err != nil {
